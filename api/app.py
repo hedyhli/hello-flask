b0d93289dd94545a1635625e17542c98c7608b15
Hedy Li <hedyhyry@gmail.com>
2020-10-27 04:43:18 +0000

    update

--- api/app.py
+++ api/app.py
@@ -30,6 +30,6 @@ def api():
         text = my_file.read()
         return text
 
-if __name__ == '__main__':
-    # running in repl.it
-  app.run(host='0.0.0.0', port=8080)
+# if __name__ == '__main__':
+#     # running in repl.it
+#   app.run(host='0.0.0.0', port=8080)
